d53a7781899792d3d31f7d99aa74bf5cdffbb048
Nobuhiro Nakashima <ombran@gmail.com>
2021-04-16 00:43:22 +0900

    add get_todo, put_todo, delete_todo

--- todo.py
+++ todo.py
@@ -19,3 +19,41 @@ def post_todo(event, context):
     response = {"statusCode": 200, "body": json.dumps(item)}
 
     return response
+
+
+def get_todo(event, context):
+    todo_id = event["pathParameters"]["todoId"]
+
+    res = table.get_item(Key={"todo_id": todo_id})
+    item = res["Item"]
+
+    response = {"statusCode": 200, "body": json.dumps(item)}
+
+    return response
+
+
+def put_todo(event, context):
+    todo_id = event["pathParameters"]["todoId"]
+    body = json.loads(event.get("body"))
+    todo = body.get("todo")
+
+    item = {"todo_id": todo_id, "todo": todo}
+    table.update_item(
+        Key={"todo_id": todo_id},
+        UpdateExpression="set todo=:todo",
+        ExpressionAttributeValues={":todo": todo},
+    )
+
+    response = {"statusCode": 200, "body": json.dumps(item)}
+
+    return response
+
+
+def delete_todo(event, context):
+    todo_id = event["pathParameters"]["todoId"]
+
+    table.delete_item(Key={"todo_id": todo_id})
+
+    response = {"statusCode": 200, "body": json.dumps({"todo_id": todo_id})}
+
+    return response
